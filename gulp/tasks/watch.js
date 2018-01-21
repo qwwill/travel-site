71f3617135e61b9825c802b7db740065728c3c19
Quincy <qw@hawaii.edu>
2018-01-20 22:58:56 -1000

    Completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -15,13 +15,21 @@ gulp.task('watch', function() {
     browserSync.reload();
   });
 
-});
+  watch('./app/assets/style/**/*.css', function() {
+    gulp.start('cssInject');
+  });
 
-watch('./app/assets/style/**/*.css', function() {
-  gulp.start('cssInject');
+  watch('./app/assets/scripts/**/*.js', function(){
+    gulp.start('scriptsRefresh');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/style.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload();
 });
